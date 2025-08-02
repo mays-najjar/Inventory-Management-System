2939ce52d992105b237dd8c0d03030fceb531cab
mays-najjar <mays.najjar2001@gmail.com>
2025-08-02 08:40:49 +0300

    start project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
